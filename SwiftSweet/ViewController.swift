5dafd397e2f42b059686bb79520837218d443c76
Carlos Duran <afg504@gmail.com>
2015-06-17 10:35:40 -0400

    Initial Commit

---
+++ SwiftSweet/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftSweet
+//
+//  Created by Carlos Duran on 6/17/15.
+//  Copyright (c) 2015 Carlos Duran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
